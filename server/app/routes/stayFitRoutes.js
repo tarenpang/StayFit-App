6449dacb1dac3c1194f115f77897e9b56187ce5c
Taren Pang <tarenpang@gmail.com>
2022-06-12 17:41:05 -0700

    added CRUD ops to controllers & routes

--- server/app/routes/stayFitRoutes.js
+++ server/app/routes/stayFitRoutes.js
@@ -1,10 +1,12 @@
 module.exports = app => {
     const exercises = require('../controllers/stayFitController')
-    var router = require('express').Router()
+    var router = require('express').Router();
 
-    router.get('/', exercises.findAll)
-    router.post('/', exercises.create)
-    
+    router.post('/', exercises.create);
+    router.get('/', exercises.findAll);
+    router.get("/:id", exercises.findOne);
+    router.put("/:id", exercises.update);
+    router.delete("/:id", exercises.delete);
 
-    app.use('/api/exercises', router)
+    app.use('/api/exercises', router);
 }
--- server/app/routes/trainerRoutes.js
+++ server/app/routes/trainerRoutes.js
@@ -2,8 +2,11 @@ module.exports = app => {
     const trainers = require('../controllers/trainerController');
     const router = require('express').Router();
 
-    router.get('/', trainers.findAll);
     router.post('/', trainers.create);
+    router.get('/', trainers.findAll);
+    router.get("/:id", trainers.findOne)
+    router.put("/:id", trainers.update);
+    router.delete("/:id", trainers.delete);
 
     app.use('/api/trainers', router); 
 }
